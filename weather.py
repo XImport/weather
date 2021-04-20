3edc161307ff7cb4c9d847052260cbb24357efd6
XImport <zed.robot99@gmail.com>
2021-04-20 18:22:04 +0000

    Now it can be compling with nuitka3 tool for linux

--- weather.py
+++ weather.py
@@ -1,7 +1,11 @@
+
 import sys
 
-from weather import Form, app
+from weather_base.style import Ui_Form
+from weather_base import Form, app
 
 if __name__ == "__main__":
+    ui = Ui_Form()
+    ui.setupUi(Form)
     Form.show()
     sys.exit(app.exec_())
--- weather_base/__init__.py
+++ weather_base/__init__.py
@@ -2,9 +2,6 @@ import sys
 
 from PyQt5 import QtWidgets
 
-from .style import Ui_Form
 
 app = QtWidgets.QApplication(sys.argv)
 Form = QtWidgets.QWidget()
-ui = Ui_Form()
-ui.setupUi(Form)
